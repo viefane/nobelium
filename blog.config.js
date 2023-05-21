c06aad6fd8d52744da9b3b6960261bd3243b4d00
viefane <suhestyle@gmail.com>
2023-05-22 06:51:43 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -17,7 +17,7 @@ const BLOG = {
   showAbout: true,
   showArchive: true,
   autoCollapsedNavBar: false, // The automatically collapsed navigation bar
-  ogImageGenerateURL: 'https://og-image-craigary.vercel.app', // The link to generate OG image, don't end with a slash
+  ogImageGenerateURL: 'https://avatars.githubusercontent.com/u/29298957?s=400&v=4', // The link to generate OG image, don't end with a slash
   socialLink: 'https://twitter.com/viefaneho',
   seo: {
     keywords: ['Blog', 'Website', 'Notion'],
