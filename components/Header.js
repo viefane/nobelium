25124f59a2807f4ec95f9fa735d6fb6d954257fa
viefane <suhestyle@gmail.com>
2023-05-29 06:43:01 +0800

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -135,7 +135,7 @@ const HeaderName = forwardRef(function HeaderName ({ siteTitle, siteDescription,
       {postTitle && <span className="post-title row-start-1 col-start-1">{postTitle}</span>}
       <span className="row-start-1 col-start-1">
         <span className="site-title">{siteTitle}</span>
-        <span className="site-description font-normal">, {siteDescription}</span>
+        <span className="site-description font-normal">{siteDescription}</span>
       </span>
     </p>
   )
