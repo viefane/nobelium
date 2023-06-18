5555b477a24be9661ec01048a85eea1c0b3c73ed
viefane <suhestyle@gmail.com>
2023-06-19 07:55:49 +0800

    font change

--- blog.config.js
+++ blog.config.js
@@ -12,7 +12,7 @@ const BLOG = {
   darkBackground: '#18181B', // use hex value, don't forget '#'
   path: '', // leave this empty unless you want to deploy Nobelium in a folder
   since: 2019, // If leave this empty, current year will be used.
-  postsPerPage: 11,
+  postsPerPage: 7,
   sortByDate: false,
   showAbout: true,
   showArchive: true,
