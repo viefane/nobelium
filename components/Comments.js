8868bb883c7fa7b43a0666a97ec715b0f645bc8b
viefane <suhestyle@gmail.com>
2023-12-07 07:43:46 +0800

    comments-font

--- components/Comments.js
+++ components/Comments.js
@@ -65,7 +65,7 @@ const Comments = ({ frontMatter }) => {
             pageUrl: BLOG.link + router.asPath,
             theme: BLOG.appearance
           }}
-          style={{ fontFamily: 'LXGWBrightGB-Regular, sans-serif' 
+          style={{ fontFamily: 'LXGWBrightGB-Regular,sans-serif !important'
          }}
         />
       )}
